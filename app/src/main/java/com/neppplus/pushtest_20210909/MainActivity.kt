21d62717d52fc64fc2a48986f817491ec2bba16c
dqwdd <dqwdd@naver.com>
2021-09-09 17:24:02 +0900

    사용자 목록 넣기 (실 데이터는 안넣음)

--- app/src/main/java/com/neppplus/pushtest_20210909/MainActivity.kt
+++ app/src/main/java/com/neppplus/pushtest_20210909/MainActivity.kt
@@ -3,8 +3,17 @@ package com.neppplus.pushtest_20210909
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.neppplus.colosseum_20210903.utils.ServerUtil
+import com.neppplus.pushtest_20210909.adapters.UserAdapter
+import com.neppplus.pushtest_20210909.datas.UserData
+import kotlinx.android.synthetic.main.activity_main.*
+import org.json.JSONObject
 
 class MainActivity : BaseActivity() {
+
+    val mUserList = ArrayList<UserData>()
+
+    lateinit var mAdapter : UserAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -19,6 +28,10 @@ class MainActivity : BaseActivity() {
     override fun setVales() {
 
         getMainData()
+
+        mAdapter = UserAdapter(mContext, R.layout.user_list_item, mUserList)
+        userListView.adapter = mAdapter
+
     }
 
     fun getMainData() {
@@ -26,6 +39,26 @@ class MainActivity : BaseActivity() {
 //        메인화면에서 쓰일 데이터 불러오기 X,
 //        기존의 v2/main_info 에 쿼리파라미터 2개 추가.
         ServerUtil.getRequestMainData(mContext, null)
+
+
+//        사용자 목록
+        ServerUtil.getRequestUserList(mContext, object : ServerUtil.JsonResponseHandler{
+            override fun onResponse(jsonObj: JSONObject) {
+
+                val dataObj = jsonObj.getJSONObject("data")
+                val userArr = dataObj.getJSONArray("users")
+
+                for (i in 0 until userArr.length() ){
+
+                    mUserList.add(UserData.getUserDataFromJson(userArr.getJSONObject(i)))
+
+                }
+
+                runOnUiThread{
+                    mAdapter.notifyDataSetChanged()
+                }
+            }
+        })
 
     }
 
