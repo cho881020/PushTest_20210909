c645f804b6dcea7a4b53a633a1d566c9b77fdfe6
r2872 <84784400+r2872@users.noreply.github.com>
2021-09-09 17:44:08 +0900

    푸시 받았을때 토스트로 본문 띄우기

--- app/src/main/java/com/neppplus/pushtest_20210909/fcm/MyFCMService.kt
+++ app/src/main/java/com/neppplus/pushtest_20210909/fcm/MyFCMService.kt
@@ -1,5 +1,8 @@
 package com.neppplus.pushtest_20210909.fcm
 
+import android.os.Handler
+import android.os.Looper
+import android.widget.Toast
 import com.google.firebase.messaging.FirebaseMessagingService
 import com.google.firebase.messaging.RemoteMessage
 
@@ -8,6 +11,11 @@ class MyFCMService : FirebaseMessagingService() {
     override fun onMessageReceived(p0: RemoteMessage) {
         super.onMessageReceived(p0)
 
+        val myHandler = Handler(Looper.getMainLooper())
 
+        myHandler.post {
+
+            Toast.makeText(applicationContext, p0.notification!!.body, Toast.LENGTH_SHORT).show()
+        }
     }
 }
