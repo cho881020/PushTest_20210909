224ecc3037b167840486de788b948c1cd94a73b7
안수지 <wwsa2@naver.com>
2021-09-09 16:08:44 +0900

    브랜치 확인용 주석 추가

--- app/src/main/java/com/neppplus/pushtest_20210909/MainActivity.kt
+++ app/src/main/java/com/neppplus/pushtest_20210909/MainActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 
 class MainActivity : BaseActivity() {
 
-//    여기는 master 브랜치입니다.
+//    여기는 안수지가 작업하는 브랜치입니다.
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
