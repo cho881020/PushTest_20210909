b0f3a33e063c04b4d0abb1fd239d11786a70f5a9
조경진 <cho881020@gmail.com>
2021-09-09 16:04:46 +0900

    테스트용 주석 추가

--- app/src/main/java/com/neppplus/pushtest_20210909/MainActivity.kt
+++ app/src/main/java/com/neppplus/pushtest_20210909/MainActivity.kt
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : BaseActivity() {
+
+//    여기는 master 브랜치입니다.
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
