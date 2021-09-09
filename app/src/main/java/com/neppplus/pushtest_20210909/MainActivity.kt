43887ba19355d15e7dcae6873eac6004bf5d8a22
Turtle <hs2352@gmail.com>
2021-09-09 16:10:53 +0900

    Update MainActivity.kt
    브랜치 변경 및 주석 추가

--- app/src/main/java/com/neppplus/pushtest_20210909/MainActivity.kt
+++ app/src/main/java/com/neppplus/pushtest_20210909/MainActivity.kt
@@ -1,9 +1,10 @@
 package com.neppplus.pushtest_20210909
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : BaseActivity() {
+    // 여기는 김희섭이 작업하는 브랜치 입니다.
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
