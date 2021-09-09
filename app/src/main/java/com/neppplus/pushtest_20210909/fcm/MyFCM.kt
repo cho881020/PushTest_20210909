830c93f9d176086237c2caa5d472652d0267e685
안수지 <wwsa2@naver.com>
2021-09-09 16:38:02 +0900

    FCM 클래스 추가 / 서비스로 앱에 등록

---
+++ app/src/main/java/com/neppplus/pushtest_20210909/fcm/MyFCM.kt
@@ -0,0 +1,14 @@
+package com.neppplus.pushtest_20210909.fcm
+
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
+class MyFCM : FirebaseMessagingService() {
+
+    override fun onMessageReceived(p0: RemoteMessage) {
+        super.onMessageReceived(p0)
+
+
+    }
+
+}
