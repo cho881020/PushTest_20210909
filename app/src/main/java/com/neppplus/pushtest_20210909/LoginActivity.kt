d7fa1c3a03e4397226c902d561f6be66df8b6046
조경진 <cho881020@gmail.com>
2021-09-09 16:52:57 +0900

    로그인 성공시 토큰 저장까지

--- app/src/main/java/com/neppplus/pushtest_20210909/LoginActivity.kt
+++ app/src/main/java/com/neppplus/pushtest_20210909/LoginActivity.kt
@@ -2,8 +2,12 @@ package com.neppplus.pushtest_20210909
 
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import com.google.firebase.iid.FirebaseInstanceId
+import com.neppplus.colosseum_20210903.utils.ContextUtil
+import com.neppplus.colosseum_20210903.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_login.*
+import org.json.JSONObject
 
 class LoginActivity : BaseActivity() {
 
@@ -27,6 +31,27 @@ class LoginActivity : BaseActivity() {
             val inputPw = passwordEdt.text.toString()
 
 //            ServerUtil클래스 활용 -> 로그인 시도.
+
+            ServerUtil.postRequestSignIn(inputId, inputPw, object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(jsonObj: JSONObject) {
+
+                    val code = jsonObj.getInt("code")
+                    if (code == 200) {
+                        val dataObj = jsonObj.getJSONObject("data")
+                        val token = dataObj.getString("token")
+                        Log.d("토큰값", token)
+
+                        ContextUtil.setToken(mContext, token)
+                    }
+                    else {
+                        runOnUiThread {
+                            Toast.makeText(mContext, "로그인 실패", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+
+                }
+
+            })
 
 
 
