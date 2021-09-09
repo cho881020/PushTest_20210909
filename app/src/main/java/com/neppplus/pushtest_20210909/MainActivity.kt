e113255739f35658e3695bbeea8b1255538a7c0a
조경진 <cho881020@gmail.com>
2021-09-09 15:57:55 +0900

    Initial commit

---
+++ app/src/main/java/com/neppplus/pushtest_20210909/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.neppplus.pushtest_20210909
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        setupEvents()
+        setVales()
+    }
+
+    override fun setupEvents() {
+
+    }
+
+    override fun setVales() {
+
+    }
+}
