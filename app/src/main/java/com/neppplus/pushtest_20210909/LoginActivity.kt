df22bb1ae0035b8725dac27e0033c0ab1ae5da7a
송병섭 <tumtum@naver.com>
2021-09-09 16:53:10 +0900

    로그인 성공 시 토큰 값 저장

--- app/src/main/java/com/neppplus/pushtest_20210909/LoginActivity.kt
+++ app/src/main/java/com/neppplus/pushtest_20210909/LoginActivity.kt
@@ -1,7 +1,11 @@
 package com.neppplus.pushtest_20210909
 
 import android.os.Bundle
+import android.widget.Toast
+import com.nepplus.colosseum_0903.utils.ContextUtil
+import com.nepplus.colosseum_0903.utils.ServerUtil
 import kotlinx.android.synthetic.main.activity_login.*
+import org.json.JSONObject
 
 class LoginActivity : BaseActivity() {
 
@@ -19,7 +23,29 @@ class LoginActivity : BaseActivity() {
         loginBtn.setOnClickListener {
             val inputId = idEdt.text.toString()
             val inputPw = pwEdt.text.toString()
+
+            ServerUtil.postRequestSignIn(inputId, inputPw, object : ServerUtil.JsonResponseHandler {
+                override fun onResponse(jsonObj: JSONObject) {
+                    val code = jsonObj.getInt("code")
+                    if(code == 200) {
+                        val dataObj = jsonObj.getJSONObject("data")
+                        val token = dataObj.getString("token")
+
+                        // 로그인 성공 시 토큰 값 저장
+                        ContextUtil.setToken(mContext, token)
+                    } else {
+                        runOnUiThread {
+                            Toast.makeText(mContext, "로그인 실패", Toast.LENGTH_SHORT).show()
+                        }
+                    }
+                }
+
+            } )
         }
+
+        // ServerUtil 클래스 활용 -> 로그인 시도
+
+
 
     }
 
